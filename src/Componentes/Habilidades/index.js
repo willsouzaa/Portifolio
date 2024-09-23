cc67bf4a2a492b90c489d867e0eb63e090317971
willsouzaa <pauloamancio79@gmail.com>
2024-09-22 21:40:09 -0300

    Alteraçao do componente Habilidades

--- src/Componentes/Habilidades/index.js
+++ src/Componentes/Habilidades/index.js
@@ -7,11 +7,35 @@ function Skills() {
         <h2 class="heading"> Minhas <strong>Habilidades</strong></h2>
 
         <div className="services-container">
-    <div className="html"><h3>HTML</h3></div>
-    <div className="css"><h3>CSS</h3></div>
-    <div className="javascript"><h3>JavaScript</h3></div>
-    <div className="react"><h3>React</h3></div>
-    <div className="git"><h3>Git</h3></div>
+        <div class="services-box">
+                <i class='bx bxl-html5' ></i>
+                <h3>HTML</h3>
+            </div>
+
+            <div class="services-box">
+                <i class='bx bxl-css3' ></i>
+                <h3>CSS</h3>
+            </div>
+
+            <div class="services-box">
+                <i class='bx bxl-javascript' ></i>
+                <h3>Javascript</h3>
+            </div>
+
+            <div class="services-box">
+                <i class='bx bxl-react'></i>
+                <h3>React</h3>
+            </div>
+
+            <div class="services-box">
+                 <i class='bx bxl-git'></i>
+                <h3>GitHub</h3>
+            </div>
+            <div class="services-box">
+                <i class='bx bxl-sql' ></i>
+                <h3>SQL</h3>
+            </div>
+    
   </div>
     </section>
   );
--- src/Componentes/Projetos/index.js
+++ src/Componentes/Projetos/index.js
@@ -2,43 +2,49 @@ import React from "react";
 import "./Projetos.css";
 import Cinetag from './cinetag.JPG';
 
-function Projetos () {
-    return(
-        <section id="projetos">
-            <h2>Meus Projetos</h2>
-            <div className="projetos-container">
-            
-                <div className="projeto">
-                <img src={Cinetag} alt="cinetag" />
-                <div className="projeto-info">
-                    <h3>Cinetag</h3>
-                    <p>Cinetag é um aplicativo web construído com React, utilizando o Create React App para configuração inicial.</p>
-                    <a href="https://cinetag-five-sigma.vercel.app/" class="btn-deploy" target="_blank">Deploy</a>
-                    <a href="https://github.com/willsouzaa/Cinetag-Router.git" class="btn-github" target="_blank">GitHub</a>
-                </div>
-                </div>
-                <div className="projeto">
-                <img src={Cinetag} alt="cinetag" />
-                <div className="projeto-info">
-                    <h3>Projeto 1 </h3>
-                    <p>Descriçao</p>
-                    <a href="https://cinetag-five-sigma.vercel.app/" class="btn-deploy" target="_blank">Deploy</a>
-                    <a href="https://github.com/willsouzaa/Cinetag-Router.git" class="btn-github" target="_blank">GitHub</a>
-                </div>
-                </div>
-                <div className="projeto">
-                <img src={Cinetag} alt="cinetag" />
-                <div className="projeto-info">
-                    <h3>Projeto 1 </h3>
-                    <p>Descriçao</p>
-                    <a href="https://cinetag-five-sigma.vercel.app/" class="btn-deploy" target="_blank">Deploy</a>
-                    <a href="https://github.com/willsouzaa/Cinetag-Router.git" class="btn-github" target="_blank">GitHub</a>
-                </div>
-                </div>
+function Projetos() {
+  return (
+    <section id="projetos">
+      <h2>Meus Projetos</h2>
+      <div className="container">
+        <div className="projeto">
+          <div className="wrapper">
+            <div className="banner-image" style={{ backgroundImage: `url(${Cinetag})` }}></div>
+            <h3>Cinetag</h3>
+            <p>Cinetag é um aplicativo web construído com React, utilizando o Create React App para configuração inicial.</p>
+          </div>
+          <div className="button-wrapper">
+            <a href="https://cinetag-five-sigma.vercel.app/" className="btn outline" target="_blank" rel="noreferrer">Deploy</a>
+            <a href="https://github.com/willsouzaa/Cinetag-Router.git" className="btn fill" target="_blank" rel="noreferrer">GitHub</a>
+          </div>
+        </div>
+        
+        <div className="projeto">
+          <div className="wrapper">
+            <div className="banner-image" style={{ backgroundImage: `url(${Cinetag})` }}></div>
+            <h3>Projeto 1</h3>
+            <p>Descrição do projeto 1.</p>
+          </div>
+          <div className="button-wrapper">
+            <a href="https://cinetag-five-sigma.vercel.app/" className="btn outline" target="_blank" rel="noreferrer">Deploy</a>
+            <a href="https://github.com/willsouzaa/Cinetag-Router.git" className="btn fill" target="_blank" rel="noreferrer">GitHub</a>
+          </div>
+        </div>
 
-            </div>
-        </section>
-    )
+        <div className="projeto">
+          <div className="wrapper">
+            <div className="banner-image" style={{ backgroundImage: `url(${Cinetag})` }}></div>
+            <h3>Projeto 2</h3>
+            <p>Descrição do projeto 2.</p>
+          </div>
+          <div className="button-wrapper">
+            <a href="https://cinetag-five-sigma.vercel.app/" className="btn outline" target="_blank" rel="noreferrer">Deploy</a>
+            <a href="https://github.com/willsouzaa/Cinetag-Router.git" className="btn fill" target="_blank" rel="noreferrer">GitHub</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projetos;
