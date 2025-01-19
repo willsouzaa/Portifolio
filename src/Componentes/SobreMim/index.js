5ca74dc526cceaea820321c60774e60acd61bfad
willsouzaa <pauloamancio79@gmail.com>
2025-01-19 14:08:45 -0300

    modificaçao de frase de apresentaçao em sobremim

--- src/Componentes/SobreMim/index.js
+++ src/Componentes/SobreMim/index.js
@@ -19,9 +19,10 @@ function AboutMe() {
             />
           </h1>
 
-          <p className='stringDescriçao'>
-            Meu nome é <strong>Paulo</strong>, sou desenvolvedor full stack com experiência em React, Node.js, JavaScript, SQL e MySQL. Aqui, você encontrará uma seleção dos meus principais projetos, desde soluções web até sistemas mais complexos, desenvolvidos ao longo da minha jornada acadêmica e como freelancer. Sinta-se à vontade para explorar meus trabalhos e entrar em contato. Será um prazer compartilhar mais sobre minhas habilidades e discutir futuras colaborações!
-          </p>
+          <p className="stringDescricao">
+  Olá! Meu nome é <strong>Paulo</strong>, estou no quarto período da graduação em Desenvolvimento Full Stack e atualmente trabalho como freelancer, desenvolvendo sites e aplicações web para ganhar experiência prática. Tenho habilidades em React, Node.js, JavaScript, SQL e MySQL, e utilizo esses conhecimentos para criar soluções eficientes e funcionais. Neste portfólio, apresento uma seleção dos meus principais projetos, que refletem minha evolução como desenvolvedor. Estou em busca de uma oportunidade de estágio para iniciar minha carreira no mercado de tecnologia. Sinta-se à vontade para explorar meus trabalhos e entrar em contato. Será um prazer colaborar com você!
+</p>
+
         </div>
         <img src={Perfil} alt="Foto do Usuário" className="profile-image" />
       </div>
